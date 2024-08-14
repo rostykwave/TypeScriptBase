1cc4e9824bef3b8e832b1a66bd43197fd6b50cf4
Rostyslav <rostykwave@gmail.com>
2024-08-14 18:37:30 +0300

    feat: tuple and comments

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+//Debugging test
 let age: number = 20;
 if (age < 50) {
   age += 10;
 }
 console.log("age", age);
 
+//Code completion
 let numbers: number[] = [];
 numbers.forEach((n) => n.toFixed);
+
+//Tuple
+const user: [number, string, boolean, number] = [1, "some", true, 1];
